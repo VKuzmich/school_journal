fe90941a3db95b1700a3bac3e07811e5703e91a7
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-10 17:38:01 +0200

    localised text and removed lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  devise_scope :user do
-    get 'logout' => 'devise/sessions#destroy'
-  end
+  devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
