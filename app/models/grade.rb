1d48bf9cd485f310abec22ea0cbd3f3c7a409bb3
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-17 19:08:07 +0200

    add validations and tests to grade

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,2 +1,8 @@
 class Grade < ApplicationRecord
+  attr_accessor :number
+  start_number = 1
+  end_number = 12
+  validates :number, presence: true
+  validates_inclusion_of :number, in: start_number..end_number
+  validates :group, presence: true, format: { with: /^(?=.*[A-Z]).+$/}
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Grade, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:number) }
+  it { should validate_inclusion_of(:number).in_range(1..12) }
 end
