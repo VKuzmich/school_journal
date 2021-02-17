b13617c304fd1ba93ae9acb7fe5b2ffb96e97765
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-17 11:03:27 +0200

    add shoulda-matchers gem and made changes in migrations

--- db/migrate/20210216131600_add_columns_to_user.rb
+++ db/migrate/20210216131600_add_columns_to_user.rb
@@ -1,8 +1,8 @@
 class AddColumnsToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name, :string, null: false
-    add_column :users, :phone, :string, null: false, limit: 15
-    add_column :users, :address, :string, null: false
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string, limit: 17
+    add_column :users, :address, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 2021_02_16_132553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "phone", limit: 15, null: false
-    t.string "address", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone", limit: 17
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
