dbf5304beeb1c908eb2d3f97c6987db38db5f571
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-18 16:20:49 +0200

    removing 2 tests

--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe Grade, type: :model do
   it { should validate_inclusion_of(:number).in_range(1..12) }
   it { should validate_presence_of(:group) }
   it { should validate_length_of(:group).is_equal_to(1)}
-  it { should_not allow_value(nil).for(:group) }
-  it { should_not allow_value('').for(:group) }
   it { should_not allow_value('d').for(:group) }
   it { should_not allow_value('SD').for(:group) }
   it { should_not allow_value('Dc').for(:group) }
