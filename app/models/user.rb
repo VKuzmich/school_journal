a7f165639bf451cd09bf817124b2857ed93d2a7f
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-18 16:00:50 +0200

    refactoring specs and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,13 @@ class User < ApplicationRecord
 
   validates :address,
             presence: true,
-            uniqueness: { case_sensitive: false },
             format: { with: /\A[a-zA-Z0-9_.,!"" ]+\z/}
 
   validates_length_of :first_name, :last_name, :address, within: 3..40
 
   validates :phone,
             presence: true,
-            format: {
-              with:  /(\+38)\(?\d{3}\)?(\d{3}[\-]\d{2}\d{2}|\d{3}-\d{4})/
-            }
+            format: { with:  /(\+38)\(?\d{3}\)?(\d{3}[\-]\d{2}\d{2}|\d{3}-\d{4})/ }
 
   validates_each :first_name, :last_name, :address do |record, attr, value|
     record.errors.add(attr) if value =~ /\A[a-z]/
