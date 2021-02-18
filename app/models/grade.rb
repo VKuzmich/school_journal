22ac211b92c9df2b89b51c470639556107a62e1c
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-18 10:20:21 +0200

    refactoring and rspec tests

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,7 +2,8 @@ class Grade < ApplicationRecord
   attr_accessor :number
   start_number = 1
   end_number = 12
+  group_letters_length = 1
   validates :number, presence: true
   validates_inclusion_of :number, in: start_number..end_number
-  validates :group, presence: true, format: { with: /^(?=.*[A-Z]).+$/}
+  validates :group, presence: true, format: { with: /\A(?=.*[A-Z]).+\z/ }, length: { is: group_letters_length}
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe Grade, type: :model do
   it { should validate_presence_of(:number) }
   it { should validate_inclusion_of(:number).in_range(1..12) }
+  it { should validate_presence_of(:group) }
+  it { should validate_length_of(:group).is_equal_to(1)}
+  it { should_not allow_value(nil).for(:group) }
+  it { should_not allow_value('').for(:group) }
+  it { should_not allow_value('d').for(:group) }
+  it { should_not allow_value('SD').for(:group) }
+  it { should_not allow_value('Dc').for(:group) }
+  it { should allow_value('D').for(:group) }
 end
