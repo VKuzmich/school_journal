edfaa9a7a112f9db880f93d5e2d3ae18157afbad
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-18 18:02:57 +0200

    validations and tests

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ApplicationRecord
   belongs_to :subject
   belongs_to :grade
-end
+
+  validates_presence_of :date_at, :home_task, :description
+  end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:date_at) }
+  it { is_expected.to validate_presence_of(:home_task) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to allow_value('2021/04/21').for(:date_at)}
+
 end
