153bd59762892ef691c105c6a0f700309f894006
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-18 16:41:50 +0200

    generate Parent model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :parents
+
   validates :first_name, :last_name,
             presence: true,
             uniqueness: { case_sensitive: false },
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_132553) do
+ActiveRecord::Schema.define(version: 2021_02_18_143832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
 
   create_table "grades", force: :cascade do |t|
     t.integer "number"
     t.string "group", limit: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "parents", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_parents_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -46,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_132553) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "parents", "users"
 end
