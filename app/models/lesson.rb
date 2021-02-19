5b0b55e3333bf90c8298b69b0d55171ebd40b3e7
Vladimir Kuzmich <vjk1976@ukr.net>
2021-02-19 10:40:42 +0200

    add relations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :subject
   belongs_to :grade
 
+  has_many :teachers
+  has_many :rates
+
   validates_presence_of :date_at, :home_task, :description
-  end
+end
