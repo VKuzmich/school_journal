1c0b134ffaf18efbe59b374e5074ba494f5a6fbd
Vladimir Kuzmich <vjk1976@ukr.net>
2021-03-03 14:54:42 +0200

    adding admin authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,14 @@
 class Admin::ApplicationController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_admin
+
+  def authenticate_admin
+    redirect_to '/', alert: 'Not authorized.' unless current_user && admin_access
+  end
+
+  private
+
+  def admin_access
+    current_user.try(:admin?)
+  end
 end
