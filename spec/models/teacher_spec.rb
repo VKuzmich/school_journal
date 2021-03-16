70708b0624fbb2e8e6866d0851da772a440cd22c
Vladimir Kuzmich <vjk1976@ukr.net>
2021-03-16 10:04:20 +0200

    refactoring test in Teacher model

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  describe '' do
-    it '#subject_name' do
-      subject = create(:subject)
-      allow_any_instance_of(Teacher).to receive(:subject_name).and_return(subject.name)
-
-      teacher = create(:teacher)
+  describe '#subject_name' do
+    let(:subject) { create(:subject) }
+    let(:teacher) { create(:teacher, subject: subject) }
+    it "returns subject's name" do
       expect(teacher.subject_name).to eq(subject.name)
     end
   end
