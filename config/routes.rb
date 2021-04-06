f9400c571fd55beb68e5fdbec62ca1e10c1e6111
VKuzmich <vjk1976@ukr.net>
2021-04-06 22:11:01 +0300

    create-journal: controller and view index for grades

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   end
 
   resources :lessons
+  resources :journals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
