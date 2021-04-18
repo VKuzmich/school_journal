e67001fb2c20e4117e80552215f10d6aa6ac604f
VKuzmich <vjk1976@ukr.net>
2021-04-18 13:44:22 +0300

    action index

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,7 +1,14 @@
 class JournalsController < ApplicationController
-
   def index
-    @grades = Grade.order('number ASC, letter')
+    if current_user&.teacher
+      @grades = Grade.order('number ASC, letter')
+    elsif current_user&.parent
+      @parent_students = Student.joins(:parent).where('parent_id': current_user)
+    elsif current_user&.student
+      redirect_to journal_path
+    else
+      redirect_to root_path
+    end
   end
 
   def show
