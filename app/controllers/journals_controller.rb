abcf97a8596a38b6a8ed9e466c1d465e87087b9d
VKuzmich <vjk1976@ukr.net>
2021-04-19 10:35:45 +0300

    refactoring3

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,7 +2,6 @@ class JournalsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    binding.pry
     render_list_of_grades if current_user.teacher?
     render_list_of_students if current_user.parent?
     return redirect_to journal_path(id: student.grade.id) if current_user&.student
