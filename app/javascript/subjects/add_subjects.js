5d602709c7cf1ed8ad1c8ac5c34f262e68b5f803
VKuzmich <vjk1976@ukr.net>
2021-06-08 09:19:48 +0300

    add subjects for teacher

---
+++ app/javascript/subjects/add_subjects.js
@@ -0,0 +1,9 @@
+$(".add_subjects").on("click", function () {
+    alert("add?");
+    // lab = $(this).text()     // get the label that was clicked
+    // $(".add_lesson").hide()      // hide all questions
+    // $(".add_lesson."+lab).show() // show questions with this label
+});
+// $(".button_to").click( function () {
+//     alert("add?");
+// })
